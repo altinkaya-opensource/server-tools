3600bb5e96b1e45f9a627d8907a58c5592910e5a
Jairo Llopis <yajo.sk8@gmail.com>
2015-11-06 17:45:53 +0100

    Split model in abstract & concrete.

--- base_multi_image/models/image.py
+++ base_multi_image/models/image.py
@@ -1,203 +1,181 @@
+# -*- coding: utf-8 -*-
 # © 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
 #        Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
 # © 2015 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import base64
-import logging
+import urllib
 import os
-from urllib.error import ContentTooShortError
-from urllib.request import urlretrieve
-
-from odoo import _, api, exceptions, fields, models, tools
+import logging
+from openerp import models, fields, api, exceptions, _
+from openerp import tools
 
 _logger = logging.getLogger(__name__)
 
 
-class Image(models.Model):
+class ImageABC(models.Model):
     _name = "base_multi_image.image"
-    # TODO: when migrating to 15.0 use image.mixin
-    _order = "sequence, owner_model, owner_id, id"
-    _description = """ image model for multiple image functionality """
     _sql_constraints = [
-        (
-            "uniq_name_owner",
-            "UNIQUE(owner_id, owner_model, name)",
-            _("A document can have only one image with the same name."),
-        ),
+        ('uniq_name_owner', 'UNIQUE(owner_id, owner_model, name)',
+         _('A document can have only one image with the same name.')),
     ]
 
-    # This Integer is really a split Many2one
-    owner_id = fields.Integer("Owner", required=True)
-    owner_model = fields.Char(required=True)
-    owner_ref_id = fields.Reference(
-        selection="_selection_owner_ref_id",
-        string="Referenced Owner",
-        compute="_compute_owner_ref_id",
-        store=True,
-    )
+    owner_id = fields.Integer(
+        "Owner",
+        required=True)
+    owner_model = fields.Char(
+        required=True)
     storage = fields.Selection(
-        [
-            ("url", "URL"),
-            ("file", "OS file"),
-            ("db", "Database"),
-            ("filestore", "Filestore"),
-        ],
+        [('url', 'URL'), ('file', 'OS file'), ('db', 'Database')],
         required=True,
-        default="filestore",
-    )
-    name = fields.Char("Image title", translate=True)
+        default='db')
+    name = fields.Char(
+        'Image title',
+        required=True,
+        translate=True)
     filename = fields.Char()
-    extension = fields.Char("File extension", readonly=True)
-    attachment_id = fields.Many2one(
-        "ir.attachment", string="Attachment", domain="[('index_content', '=', 'image')]"
-    )
-    file_db_store = fields.Binary("Image stored in database")
-    path = fields.Char("Image path", help="Image path")
-    url = fields.Char("Image remote URL")
-    image_main = fields.Image("Full-sized image", compute="_compute_image")
-    image_medium = fields.Image(
+    extension = fields.Char(
+        'File extension',
+        readonly=True)
+    file_db_store = fields.Binary(
+        'Image stored in database',
+        filters='*.png,*.jpg,*.gif')
+    path = fields.Char(
+        "Image path",
+        help="Image path")
+    url = fields.Char(
+        'Image remote URL')
+    image_main = fields.Binary(
+        "Full-sized image",
+        compute="_get_image")
+    image_medium = fields.Binary(
         "Medium-sized image",
-        related="image_main",
-        max_width=128,
-        max_height=128,
+        compute="_get_image_sizes",
         help="Medium-sized image. It is automatically resized as a "
-        "128 x 128 px image, with aspect ratio preserved, only when the "
-        "image exceeds one of those sizes. Use this field in form views "
-        "or kanban views.",
-    )
-    image_small = fields.Image(
+             "128 x 128 px image, with aspect ratio preserved, only when the "
+             "image exceeds one of those sizes. Use this field in form views "
+             "or kanban views.")
+    image_small = fields.Binary(
         "Small-sized image",
-        related="image_main",
-        max_width=64,
-        max_height=64,
+        compute="_get_image_sizes",
         help="Small-sized image. It is automatically resized as a 64 x 64 px "
-        "image, with aspect ratio preserved. Use this field anywhere a "
-        "small image is required.",
-    )
-    comments = fields.Text(translate=True)
-    sequence = fields.Integer(default=10)
-    show_technical = fields.Boolean(compute="_compute_show_technical")
+             "image, with aspect ratio preserved. Use this field anywhere a "
+             "small image is required.")
+    comments = fields.Text(
+        'Comments',
+        translate=True)
+    show_technical = fields.Boolean(
+        compute="_show_technical")
 
-    @api.model
-    @tools.ormcache("self")
-    def _selection_owner_ref_id(self):
-        """Allow any model; after all, this field is readonly."""
-        return [(r.model, r.name) for r in self.env["ir.model"].search([])]
-
-    @api.depends("owner_model", "owner_id")
-    def _compute_owner_ref_id(self):
-        """Get a reference field based on the split model and id fields."""
-        for s in self:
-            if s.owner_model:
-                s.owner_ref_id = "{0.owner_model},{0.owner_id}".format(s)
-
-    @api.depends("storage", "path", "file_db_store", "url")
-    def _compute_image(self):
+    @api.multi
+    @api.depends('storage', 'path', 'file_db_store', 'url')
+    def _get_image(self):
         """Get image data from the right storage type."""
         for s in self:
             s.image_main = getattr(s, "_get_image_from_%s" % s.storage)()
 
+    @api.multi
     @api.depends("owner_id", "owner_model")
-    def _compute_show_technical(self):
+    def _show_technical(self):
         """Know if you need to show the technical fields."""
         self.show_technical = all(
-            "default_owner_%s" % f not in self.env.context for f in ("id", "model")
-        )
+            "default_owner_%s" % f not in self.env.context
+            for f in ("id", "model"))
 
-    def _get_image_from_filestore(self):
-        return self.attachment_id.datas
-
+    @api.multi
     def _get_image_from_db(self):
         return self.file_db_store
 
+    @api.multi
     def _get_image_from_file(self):
         if self.path and os.path.exists(self.path):
             try:
-                with open(self.path, "rb") as f:
+                with open(self.path, 'rb') as f:
                     return base64.b64encode(f.read())
             except Exception as e:
-                _logger.error(
-                    "Can not open the image %s, error : %s", self.path, e, exc_info=True
-                )
+                _logger.error("Can not open the image %s, error : %s",
+                              self.path, e, exc_info=True)
         else:
             _logger.error("The image %s doesn't exist ", self.path)
 
         return False
 
+    @api.multi
     def _get_image_from_url(self):
-        return self._get_image_from_url_cached(self.url)
-
-    @api.model
-    @tools.ormcache("url")
-    def _get_image_from_url_cached(self, url):
-        """Allow to download an image and cache it by its URL."""
-        if url:
+        if self.url:
             try:
-                (filename, header) = urlretrieve(url)
-                with open(filename, "rb") as f:
+                (filename, header) = urllib.urlretrieve(self.url)
+                with open(filename, 'rb') as f:
                     return base64.b64encode(f.read())
-            except ContentTooShortError:
-                _logger.error("URL %s cannot be fetched", url, exc_info=True)
+            except:
+                _logger.error("URL %s cannot be fetched", self.url,
+                              exc_info=True)
 
         return False
 
-    @api.model
-    def _make_name_pretty(self, name):
-        return name.replace("_", " ").capitalize()
+    @api.multi
+    @api.depends('image_main')
+    def _get_image_sizes(self):
+        for s in self:
+            try:
+                vals = tools.image_get_resized_images(
+                    s.with_context(bin_size=False).image_main)
+            except:
+                vals = {"image_medium": False,
+                        "image_small": False}
+            s.update(vals)
 
-    @api.onchange("url")
+    @api.multi
+    def _check_filestore(self):
+        """check if the filestore is created, and do it otherwise."""
+        for s in self:
+            dir_path = os.path.dirname(s.path)
+            try:
+                if not os.path.exists(dir_path):
+                    os.makedirs(dir_path)
+            except OSError as e:
+                raise exceptions.Warning(
+                    _('The image filestore cannot be created, %s') % e)
+
+    @api.model
+    def _make_pretty(self, name):
+        return name.replace('_', ' ').capitalize()
+
+    @api.onchange('url')
     def _onchange_url(self):
         if self.url:
-            filename = self.url.split("/")[-1]
+            filename = self.url.split('/')[-1]
             self.name, self.extension = os.path.splitext(filename)
-            self.name = self._make_name_pretty(self.name)
+            self.name = self._make_pretty(self.name)
 
-    @api.onchange("path")
+    @api.onchange('path')
     def _onchange_path(self):
         if self.path:
-            self.name, self.extension = os.path.splitext(os.path.basename(self.path))
-            self.name = self._make_name_pretty(self.name)
+            self.name, self.extension = os.path.splitext(os.path.basename(
+                self.path))
+            self.name = self._make_pretty(self.name)
 
-    @api.onchange("filename")
+    @api.onchange('filename')
     def _onchange_filename(self):
         if self.filename:
             self.name, self.extension = os.path.splitext(self.filename)
-            self.name = self._make_name_pretty(self.name)
+            self.name = self._make_pretty(self.name)
 
-    @api.onchange("attachment_id")
-    def _onchange_attachmend_id(self):
-        if self.attachment_id:
-            self.name = self.attachment_id.res_name
-
-    @api.constrains("storage", "url")
+    @api.constrains('storage', 'url')
     def _check_url(self):
-        for record in self:
-            if record.storage == "url" and not record.url:
-                raise exceptions.ValidationError(
-                    _("You must provide an URL for the image.")
-                )
+        if self.storage == 'url' and not self.url:
+            raise exceptions.ValidationError(
+                'You must provide an URL for the image.')
 
-    @api.constrains("storage", "path")
+    @api.constrains('storage', 'path')
     def _check_path(self):
-        for record in self:
-            if record.storage == "file" and not record.path:
-                raise exceptions.ValidationError(
-                    _("You must provide a file path for the image.")
-                )
+        if self.storage == 'file' and not self.path:
+            raise exceptions.ValidationError(
+                'You must provide a file path for the image.')
 
-    @api.constrains("storage", "file_db_store")
+    @api.constrains('storage', 'file_db_store')
     def _check_store(self):
-        for record in self:
-            if record.storage == "db" and not record.file_db_store:
-                raise exceptions.ValidationError(
-                    _("You must provide an attached file for the image.")
-                )
-
-    @api.constrains("storage", "attachment_id")
-    def _check_attachment_id(self):
-        for record in self:
-            if record.storage == "filestore" and not record.attachment_id:
-                raise exceptions.ValidationError(
-                    _("You must provide an attachment for the image.")
-                )
+        if self.storage == 'db' and not self.file_db_store:
+            raise exceptions.ValidationError(
+                'You must provide an attached file for the image.')
--- base_multi_image/models/owner.py
+++ base_multi_image/models/owner.py
@@ -1,54 +1,55 @@
+# -*- coding: utf-8 -*-
 # © 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
 #        Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
 # © 2015 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import _, api, fields, models, tools
+from openerp import _, api, fields, models, tools
 
 
 class Owner(models.AbstractModel):
     _name = "base_multi_image.owner"
-    _description = """ Wizard for base multi image """
 
     image_ids = fields.One2many(
-        comodel_name="base_multi_image.image",
-        inverse_name="owner_id",
-        string="Images",
+        comodel_name='base_multi_image.image',
+        inverse_name='owner_id',
+        string='Images',
         domain=lambda self: [("owner_model", "=", self._name)],
-        copy=True,
-    )
+        copy=True)
     image_main = fields.Binary(
         string="Main image",
         store=False,
-        compute="_compute_multi_image",
-        inverse="_inverse_multi_image_main",
-    )
+        compute="_get_multi_image",
+        inverse="_set_multi_image_main")
     image_main_medium = fields.Binary(
         string="Medium image",
-        compute="_compute_multi_image",
-        inverse="_inverse_multi_image_main_medium",
-        store=False,
-    )
+        compute="_get_multi_image",
+        inverse="_set_multi_image_main_medium",
+        store=False)
     image_main_small = fields.Binary(
         string="Small image",
-        compute="_compute_multi_image",
-        inverse="_inverse_multi_image_main_small",
-        store=False,
-    )
+        compute="_get_multi_image",
+        inverse="_set_multi_image_main_small",
+        store=False)
 
-    @api.depends("image_ids")
-    def _compute_multi_image(self):
-        """Get the main image for this object.
+    @api.multi
+    @api.depends('image_ids')
+    def _get_multi_image(self):
+        """Get a the main image for this object.
 
         This is provided as a compatibility layer for submodels that already
         had one image per record.
         """
         for s in self:
-            first = s.image_ids[:1]
-            s.image_main = first.image_main
-            s.image_main_medium = first.image_medium
-            s.image_main_small = first.image_small
+            s.image_main = False
+            s.image_main_medium = False
+            s.image_main_small = False
+            if s.image_ids:
+                s.image_main = s.image_ids[0].image_main
+                s.image_main_medium = s.image_ids[0].image_medium
+                s.image_main_small = s.image_ids[0].image_small
 
+    @api.multi
     def _set_multi_image(self, image=False, name=False):
         """Save or delete the main image for this record.
 
@@ -58,7 +59,7 @@ class Owner(models.AbstractModel):
         # Values to save
         values = {
             "storage": "db",
-            "file_db_store": tools.image_process(image, size=(1024, 1024)),
+            "file_db_store": tools.image_resize_image_big(image),
             "owner_model": self._name,
         }
         if name:
@@ -78,22 +79,23 @@ class Owner(models.AbstractModel):
             elif s.image_ids:
                 s.image_ids[0].unlink()
 
-    def _inverse_multi_image_main(self):
+    @api.multi
+    def _set_multi_image_main(self):
         self._set_multi_image(self.image_main)
 
-    def _inverse_multi_image_main_medium(self):
+    @api.multi
+    def _set_multi_image_main_medium(self):
         self._set_multi_image(self.image_main_medium)
 
-    def _inverse_multi_image_main_small(self):
+    @api.multi
+    def _set_multi_image_main_small(self):
         self._set_multi_image(self.image_main_small)
 
+    @api.multi
     def unlink(self):
-        """Mimic `ondelete="cascade"` for multi images.
-
-        Will be skipped if ``env.context['bypass_image_removal']`` == True
-        """
+        """Mimic `ondelete="cascade"` for multi images."""
         images = self.mapped("image_ids")
-        result = super().unlink()
-        if result and not self.env.context.get("bypass_image_removal"):
+        result = super(Owner, self).unlink()
+        if result:
             images.unlink()
         return result
