21b765567920ccb30b771e25fedab8569650bceb
erlinberg <develierol@gmail.com>
2025-05-01 14:09:33 +0300

    [MIG] Migrate base_multi_image with needed adjustements

--- base_multi_image/models/image.py
+++ base_multi_image/models/image.py
@@ -4,19 +4,31 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import base64
+import io
 import logging
 import os
-from urllib.error import ContentTooShortError
-from urllib.request import urlretrieve
+import re
 
-from odoo import _, api, exceptions, fields, models, tools
+import requests
+from PIL import Image as PILImage
+
+from odoo import _, api, fields, models, tools
+from odoo.exceptions import UserError
+from odoo.tools import config
+
+from odoo.addons.base_import.models.base_import import (
+    DEFAULT_IMAGE_CHUNK_SIZE,
+    DEFAULT_IMAGE_MAXBYTES,
+    DEFAULT_IMAGE_REGEX,
+    DEFAULT_IMAGE_TIMEOUT,
+)
 
 _logger = logging.getLogger(__name__)
 
 
 class Image(models.Model):
     _name = "base_multi_image.image"
-    # TODO: when migrating to 15.0 use image.mixin
+    _inherit = "image.mixin"
     _order = "sequence, owner_model, owner_id, id"
     _description = """ image model for multiple image functionality """
     _sql_constraints = [
@@ -28,7 +40,7 @@ class Image(models.Model):
     ]
 
     # This Integer is really a split Many2one
-    owner_id = fields.Integer("Owner", required=True)
+    owner_id = fields.Integer(string="Owner", required=True)
     owner_model = fields.Char(required=True)
     owner_ref_id = fields.Reference(
         selection="_selection_owner_ref_id",
@@ -36,44 +48,11 @@ class Image(models.Model):
         compute="_compute_owner_ref_id",
         store=True,
     )
-    storage = fields.Selection(
-        [
-            ("url", "URL"),
-            ("file", "OS file"),
-            ("db", "Database"),
-            ("filestore", "Filestore"),
-        ],
-        required=True,
-        default="filestore",
-    )
-    name = fields.Char("Image title", translate=True)
-    filename = fields.Char()
-    extension = fields.Char("File extension", readonly=True)
-    attachment_id = fields.Many2one(
-        "ir.attachment", string="Attachment", domain="[('index_content', '=', 'image')]"
-    )
-    file_db_store = fields.Binary("Image stored in database")
-    path = fields.Char("Image path", help="Image path")
-    url = fields.Char("Image remote URL")
-    image_main = fields.Image("Full-sized image", compute="_compute_image")
-    image_medium = fields.Image(
-        "Medium-sized image",
-        related="image_main",
-        max_width=128,
-        max_height=128,
-        help="Medium-sized image. It is automatically resized as a "
-        "128 x 128 px image, with aspect ratio preserved, only when the "
-        "image exceeds one of those sizes. Use this field in form views "
-        "or kanban views.",
-    )
-    image_small = fields.Image(
-        "Small-sized image",
-        related="image_main",
-        max_width=64,
-        max_height=64,
-        help="Small-sized image. It is automatically resized as a 64 x 64 px "
-        "image, with aspect ratio preserved. Use this field anywhere a "
-        "small image is required.",
+    name = fields.Char(string="Image title", translate=True)
+    load_from = fields.Char(
+        string="Load from",
+        help="Either a remote url or a file path on the server",
+        store=False,
     )
     comments = fields.Text(translate=True)
     sequence = fields.Integer(default=10)
@@ -83,7 +62,7 @@ class Image(models.Model):
     @tools.ormcache("self")
     def _selection_owner_ref_id(self):
         """Allow any model; after all, this field is readonly."""
-        return [(r.model, r.name) for r in self.env["ir.model"].sudo().search([])]
+        return [(r.model, r.name) for r in self.env["ir.model"].search([])]
 
     @api.depends("owner_model", "owner_id")
     def _compute_owner_ref_id(self):
@@ -92,12 +71,6 @@ class Image(models.Model):
             if s.owner_model:
                 s.owner_ref_id = "{0.owner_model},{0.owner_id}".format(s)
 
-    @api.depends("storage", "path", "file_db_store", "url")
-    def _compute_image(self):
-        """Get image data from the right storage type."""
-        for s in self:
-            s.image_main = getattr(s, "_get_image_from_%s" % s.storage)()
-
     @api.depends("owner_id", "owner_model")
     def _compute_show_technical(self):
         """Know if you need to show the technical fields."""
@@ -105,163 +78,110 @@ class Image(models.Model):
             "default_owner_%s" % f not in self.env.context for f in ("id", "model")
         )
 
-    def _get_image_from_filestore(self):
-        return self.attachment_id.datas
-
-    def _get_image_from_db(self):
-        return self.file_db_store
-
-    def _get_image_from_file(self):
-        if self.path and os.path.exists(self.path):
-            try:
-                with open(self.path, "rb") as f:
-                    return base64.b64encode(f.read())
-            except Exception as e:
-                _logger.error(
-                    "Can not open the image %s, error : %s", self.path, e, exc_info=True
-                )
+    @api.onchange("load_from")
+    def _onchange_load_from(self):
+        if not self.load_from:
+            return
+        if re.match(
+            config.get("import_image_regex", DEFAULT_IMAGE_REGEX), self.load_from
+        ):
+            # Retrieve from remote url
+            self.image_1920 = self._get_image_from_url(self.load_from)
+            filename = self.load_from.split("/")[-1]
+            self.name = os.path.splitext(filename)[0]
         else:
-            _logger.error("The image %s doesn't exist ", self.path)
-
-        return False
-
-    def _get_image_from_url(self):
-        return self._get_image_from_url_cached(self.url)
+            self.image_1920 = self._get_image_from_file(self.load_from)
+            self.name = os.path.splitext(os.path.basename(self.load_from))[0]
+        self.name = self._make_name_pretty(self.name)
+        self.load_from = False
 
     @api.model
-    @tools.ormcache("url")
-    def _get_image_from_url_cached(self, url):
-        """Allow to download an image and cache it by its URL."""
-        if url:
+    def _get_image_from_file(self, path):
+        allowed_paths = tools.config.get("readable_folders", "")
+        if not allowed_paths:
+            raise UserError(_("The image %s doesn't exist", path))
+        allowed_paths = allowed_paths and allowed_paths.split(",") or []
+        file_path = os.path.abspath(os.path.expanduser(os.path.expandvars(path)))
+
+        if (
+            file_path
+            and os.path.exists(file_path)
+            and any(file_path.startswith(p) for p in allowed_paths)
+        ):
             try:
-                (filename, header) = urlretrieve(url)
-                with open(filename, "rb") as f:
+                with open(file_path, "rb") as f:
                     return base64.b64encode(f.read())
-            except ContentTooShortError:
-                _logger.error("URL %s cannot be fetched", url, exc_info=True)
+            except Exception as e:
+                raise UserError(
+                    _(
+                        "Can not open the image %(path)s, error : %(error)s",
+                        path=path,
+                        error=e,
+                        exc_info=True,
+                    )
+                ) from e
+        else:
+            raise UserError(_("The image %s doesn't exist", path))
 
         return False
+
+    @api.model
+    def _get_image_from_url(self, url):
+        with requests.Session() as session:
+            session.stream = True
+            # Same code as base_import._import_image_by_url
+            maxsize = int(config.get("import_image_maxbytes", DEFAULT_IMAGE_MAXBYTES))
+            try:
+                response = session.get(
+                    url,
+                    timeout=int(
+                        config.get("import_image_timeout", DEFAULT_IMAGE_TIMEOUT)
+                    ),
+                )
+                response.raise_for_status()
+
+                if (
+                    response.headers.get("Content-Length")
+                    and int(response.headers["Content-Length"]) > maxsize
+                ):
+                    raise UserError(
+                        _("File size exceeds configured maximum (%s bytes)", maxsize)
+                    )
+
+                content = bytearray()
+                for chunk in response.iter_content(DEFAULT_IMAGE_CHUNK_SIZE):
+                    content += chunk
+                    if len(content) > maxsize:
+                        raise UserError(
+                            _(
+                                "File size exceeds configured maximum (%s bytes)",
+                                maxsize,
+                            )
+                        )
+
+                image = PILImage.open(io.BytesIO(content))
+                w, h = image.size
+                if w * h > 42e6:  # Nokia Lumia 1020 photo resolution
+                    raise UserError(
+                        _(
+                            "Image size excessive, "
+                            "imported images must be smaller than 42 million pixel"
+                        )
+                    )
+
+                return base64.b64encode(content)
+            except Exception as e:
+                _logger.exception(e)
+                raise UserError(
+                    _("Could not retrieve URL: %(url)s: %(error)s", url=url, error=e)
+                ) from e
 
     @api.model
     def _make_name_pretty(self, name):
         return name.replace("_", " ").capitalize()
 
-    @api.onchange("url")
-    def _onchange_url(self):
-        if self.url:
-            filename = self.url.split("/")[-1]
-            self.name, self.extension = os.path.splitext(filename)
-            self.name = self._make_name_pretty(self.name)
-
-    @api.onchange("path")
-    def _onchange_path(self):
-        if self.path:
-            self.name, self.extension = os.path.splitext(os.path.basename(self.path))
-            self.name = self._make_name_pretty(self.name)
-
     @api.onchange("filename")
     def _onchange_filename(self):
         if self.filename:
-            self.name, self.extension = os.path.splitext(self.filename)
+            self.name = os.path.splitext(self.filename)[0]
             self.name = self._make_name_pretty(self.name)
-
-    @api.onchange("attachment_id")
-    def _onchange_attachmend_id(self):
-        if self.attachment_id:
-            self.name = self.attachment_id.res_name
-
-    @api.constrains("storage", "url")
-    def _check_url(self):
-        for record in self:
-            if record.storage == "url" and not record.url:
-                raise exceptions.ValidationError(
-                    _("You must provide an URL for the image.")
-                )
-
-    @api.constrains("storage", "path")
-    def _check_path(self):
-        for record in self:
-            if record.storage == "file" and not record.path:
-                raise exceptions.ValidationError(
-                    _("You must provide a file path for the image.")
-                )
-
-    @api.constrains("storage", "file_db_store")
-    def _check_store(self):
-        for record in self:
-            if record.storage == "db" and not record.file_db_store:
-                raise exceptions.ValidationError(
-                    _("You must provide an attached file for the image.")
-                )
-
-    @api.constrains("storage", "attachment_id")
-    def _check_attachment_id(self):
-        for record in self:
-            if record.storage == "filestore" and not record.attachment_id:
-                raise exceptions.ValidationError(
-                    _("You must provide an attachment for the image.")
-                )
-
-    def action_move_images_to_filestore(self):
-        """
-        Low level method that automatically converts DB images into filestore to save space.
-        It is not intended to be called directly, but from a cron job.
-        We are using search_read instead of search to avoid prefetching the image data.
-        """
-        db_images = self.search([("storage", "=", "db")])
-        for image in db_images:
-            image.sudo().with_delay()._convert_to_filestore()
-        return True
-
-    def _convert_to_filestore(self):
-        """
-        Single method that automatically converts DB images into filestore to save space.
-        """
-        self.ensure_one()
-        Attachment = self.env["ir.attachment"]
-        image_dict = self.search_read(
-            [("id", "=", self.id)], ["file_db_store", "name"], limit=1
-        )[0]
-        image_data = image_dict.get("file_db_store")
-        image_name = image_dict.get("name") or self.owner_ref_id.display_name
-        if not image_data:
-            _logger.warning(
-                "Convert DB images to filestore: Image %s has no data",
-                image_name,
-            )
-            return False
-
-        if not image_name:
-            _logger.warning(
-                "Convert DB images to filestore: Image %s has no name",
-                image_name,
-            )
-            return False
-
-        # Create the attachment
-        attachment = Attachment.create(
-            {
-                "type": "binary",
-                "name": image_name,
-                "datas": image_data,
-                "store_fname": self.filename,
-                ## Adding reference to the image owner is not necessary,
-                ## it also causes name change on the image
-                # "res_model": image.owner_model,
-                # "res_id": image.owner_id,
-                "index_content": "image",
-            }
-        )
-        # Update the image
-        self.write(
-            {
-                "attachment_id": attachment.id,
-                "filename": attachment.store_fname,
-                "storage": "filestore",
-                "file_db_store": False,
-            }
-        )
-        self._onchange_attachmend_id()
-        _logger.info("Convert DB images to filestore: Image %s converted", image_name)
-
-        return True
